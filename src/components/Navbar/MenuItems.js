8645be9203c6dbea82585973082aacd57127710b
keungsong <80748921+keungsong@users.noreply.github.com>
2021-08-28 22:02:34 +0700

    first

---
+++ src/components/Navbar/MenuItems.js
@@ -0,0 +1,42 @@
+export const MenuItems = [
+    {
+        title:"Home",
+        url:'#',
+        cName:'nav-links'
+    },
+    {
+        title:"Who we are",
+        url:'#',
+        cName:'nav-links'
+    },
+    {
+        title:"Services",
+        url:'#',
+        cName:'nav-links'
+    },
+    {
+        title:"Contact us",
+        url:'#',
+        cName:'nav-links'
+    },
+    {
+        title:"Home",
+        url:'#',
+        cName:'nav-links'
+    },
+    {
+        title:"Home",
+        url:'#',
+        cName:'nav-links'
+    },
+    {
+        title:"Home",
+        url:'#',
+        cName:'nav-links'
+    },
+    {
+        title:"Sign up",
+        url:'#',
+        cName:'nav-links'
+    },
+]
